84c3f21aa53e05d18dac8a11366753aec16f121e
baehynebin <baehynebin@hotmail.co.kr>
2017-09-20 22:49:02 +0900

    스플래시 시간 단축

--- app/src/main/java/hansei/purplebeen/com/hansei_app/controller/activitys/SplashActivity.java
+++ app/src/main/java/hansei/purplebeen/com/hansei_app/controller/activitys/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
 
         Thread t = new Thread(() -> {
             try {
-                Thread.sleep(4000);
+                Thread.sleep(3000);
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
             } catch (InterruptedException e) {
                 e.printStackTrace();
