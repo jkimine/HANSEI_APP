7a816e991cc5128fc1e86a34061797a9208ac290
baehyeonbin <baehynebin@hotmail.co.kr>
2017-09-20 12:58:00 +0900

    홍보영상 재생기능 설정

--- app/src/main/java/hansei/purplebeen/com/hansei_app/controller/fragments/IntroduceVideoFragment.java
+++ app/src/main/java/hansei/purplebeen/com/hansei_app/controller/fragments/IntroduceVideoFragment.java
@@ -1,16 +1,29 @@
 package hansei.purplebeen.com.hansei_app.controller.fragments;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import hansei.purplebeen.com.hansei_app.R;
 
 public class IntroduceVideoFragment extends Fragment {
+
+    private View view;
+    private ImageButton playButton;
+    private Uri uri;
+    private static String VIDEO_URL="http://purplebeen.uy.to:8080/resources/images/hansei_video.mp4";
     public IntroduceVideoFragment() {
         // Required empty public constructor
     }
@@ -19,7 +32,32 @@ public class IntroduceVideoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_introduce_video, container, false);
+        view = inflater.inflate(R.layout.fragment_introduce_video, container, false);
+        init();
+        loadVideo();
+        return view;
+
     }
 
+    private void init() {
+        playButton = (ImageButton) view.findViewById(R.id.playButton);
+    }
+
+    private void loadVideo() {
+        uri = Uri.parse(VIDEO_URL);
+        playButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+            intent.setDataAndType(uri,"video/*");
+            startActivityForResult(intent,3000);
+        });
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 3000) {
+            playButton.setBackground(getActivity().getDrawable(R.drawable.ic_refresh));
+        }
+    }
 }
